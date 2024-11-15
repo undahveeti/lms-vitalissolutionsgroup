3a57f13e692cb2b1f5a8a9fb11c36e2c703548cf
Andy Tran <undahvt@gmail.com>
2024-11-14 19:28:22 -0800

    logo place holder implementation

--- vitalis-solutions-group-courses/app/(dashboard)/_components/sidebar.tsx
+++ vitalis-solutions-group-courses/app/(dashboard)/_components/sidebar.tsx
@@ -1,7 +1,11 @@
+import { Logo } from "./logo"
+
 export const Sidebar = () => {
     return (
         <div className = "h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
-            SideBar!
+             <div className = "p-6">
+                <Logo />
+             </div>
         </div>
     )
 }
