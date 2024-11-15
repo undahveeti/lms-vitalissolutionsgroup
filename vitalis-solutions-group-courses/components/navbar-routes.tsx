e9b6b4e1c426d9b79b669bb0db09f2341789b0c6
Andy Tran <undahvt@gmail.com>
2024-11-15 13:35:08 -0800

    exit and teacher mode rerouting

--- vitalis-solutions-group-courses/components/navbar-routes.tsx
+++ vitalis-solutions-group-courses/components/navbar-routes.tsx
@@ -2,7 +2,7 @@
 
 import { UserButton } from "@clerk/nextjs";
 
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname} from "next/navigation";
 
 import { Button } from "@/components/ui/button";
 import { LogOut } from "lucide-react";
@@ -13,7 +13,6 @@ import Link from "next/link";
 export const NavbarRoutes = () => {
 
     const pathname = usePathname();
-    const router = useRouter();
 
     const isTeacherPage = pathname?.startsWith("/teacher");
     const isPlayerPage = pathname.includes("/chapter");
@@ -21,10 +20,12 @@ export const NavbarRoutes = () => {
     return (
         <div className = "flex gap-x-2 ml-auto">
             {isTeacherPage || isPlayerPage ? (
-                <Button size="sm" variant="ghost">
-                    <LogOut className="h-4 w-4 mr-2" />
-                    Exit
-                </Button>
+                <Link href="/">
+                    <Button size="sm" variant="ghost">
+                        <LogOut className="h-4 w-4 mr-2" />
+                        Exit
+                    </Button>
+                </Link>
             ): (
                 <Link href="/teacher/courses">
                     <Button size="sm" variant="ghost">
