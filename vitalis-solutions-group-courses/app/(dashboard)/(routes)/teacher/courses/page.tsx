364a6c1ac15a2e5567b35e46005d71548f0356c2
Andy Tran <undahvt@gmail.com>
2024-11-15 14:08:21 -0800

    create page with functionality

--- vitalis-solutions-group-courses/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ vitalis-solutions-group-courses/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const CoursesPage = () => {
     return ( 
         <div>
-            Courses page!
+            <Link href = "/teacher/create">
+                <Button>
+                    New Course
+                </Button>
+            </Link>
         </div>
      );
 }
